8ecee1eada6468fddacc551a8a319d88d58651dd
CodeNext <codenext@admins-MacBook-Pro.local>
2018-10-13 15:43:04 -0400

    Update data.js
    yamila

--- data.js
+++ data.js
@@ -1,5 +1,30 @@
-// Example
 let message = "Hello";
 console.log(message);
 
-// Type your code below!
+let hasnofavcolor = true;
+console.log(hasnofavcolor);
+
+let is14yearsold = true;
+console.log(is14yearsold);
+
+let hasacat = false;
+console.log(hasacat);
+
+let numberofsiblings = 0;
+console.log(numberofsiblings);
+
+let numberofpets = 1;
+console.log(numberofpets);
+
+let grade = 9;
+console.log(grade);
+
+let name = "Yamila"
+console.log(name);
+
+
+let middlename = "Hazel"
+console.log(middlename);
+
+let lastname = "Rangel"
+console.log(lastname);

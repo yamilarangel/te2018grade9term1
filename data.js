947a4f1d47ebc3e25760896a342d0b7e5129ef48
Akeena Hall <akeenadhall@gmail.com>
2018-10-13 15:42:41 -0400

    Update: Adding 3 booleans, 3 strings, and 3 numbers

--- data.js
+++ data.js
@@ -1,5 +1,18 @@
 //Example "y";
 let message = "Heyyyyyy"
 console.log(message);
-
-// Type your code below!
+let doesLikePie= false;
+console.log(doesLikePie)
+let name= "Akeena"
+console.log(name)
+let number= 3*3
+console.log(number)
+let doesWatchSupernatural= true;
+console.log(doesWatchSupernatural)
+let number2= 14
+let age= "14"
+console.log(number)
+let yearBorn= 2004 
+console.log(yearBorn)
+let weatheFeelsLike= -47;
+console.log(weatheFeelsLike)
